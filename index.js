98d55a3fa5ce55429cd5f7b7154ae352ebc70482
Parry Huang <parryhuang94@gmail.com>
2022-08-09 16:06:28 -0400

    commit tree c2dcac67783f2ac975c0ffc70ef0af4515a0b752

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name){
+    cats.push(name);
+}
+
+function destructivelyPrependCat(name){
+    cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat(){
+    cats.pop();
+}
+
+function destructivelyRemoveFirstCat(){
+    cats.shift();
+}
+
+function appendCat(name){
+    const newCats = [...cats];
+    newCats.push(name);
+    return newCats;
+}
+
+function prependCat(name){
+    const newCats = [...cats];
+    newCats.unshift(name);
+    return newCats;
+}
+
+function removeLastCat(name){
+    const newCats = [...cats];
+    newCats.pop(name);
+    return newCats;
+}
+
+function removeFirstCat(name){
+    const newCats = [...cats];
+    newCats.shift(name);
+    return newCats;
+}
